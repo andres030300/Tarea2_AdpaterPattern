0cd58b0a4b0afdb25c4aa21cb810159fbf67d906
andres030300 <43822019+andres030300@users.noreply.github.com>
2020-05-27 20:50:39 -0600

    Añadí ejemplo completo adapter patter
    pato

---
+++ src/PatoJuguetedePlastico.java
@@ -0,0 +1,13 @@
+public class PatoJuguetedePlastico implements PatoJuguete  {
+    //Implementa el PatoJuguete
+    public void chirrido() {
+
+        /*Esta función imprime en la terminal un mensaje de "chirrido", lo cual se refiere a que el pato de juguete está haciendo un sonido cuando lo aprietan.
+         *@author Andrés Quirós
+         *@Version 24/05/2020
+         * @param nada
+         */
+
+        System.out.println("chirrido");
+    }
+}
