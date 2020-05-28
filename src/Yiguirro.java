f9968e26a9213c73214d489c56fae2d62806f2cd
andres030300 <43822019+andres030300@users.noreply.github.com>
2020-05-27 20:50:14 -0600

    Añadí ejemplo completo adapter patter
    yiguirro

---
+++ src/Yiguirro.java
@@ -0,0 +1,20 @@
+public class Yiguirro implements Pajaro {
+
+
+    public void volar() {
+        /*Esta función imprime en la terminal un mensaje de "volando23, lo cual se refiere a que el yiguirro está volando.
+         *@author Andrés Quirós
+         *@Version 24/05/2020
+         * @param nada
+         */
+        System.out.println("Volando");
+    }
+    public void hacerSonido() {
+        /*Esta función imprime en la terminal un mensaje de "chirp chirp", lo cual se refiere a que el yiguirro está haciendo sonidos de pájaro.
+         *@author Andrés Quirós
+         *@Version 24/05/2020
+         * @param nada
+         */
+        System.out.println("Chirp Chirp");
+    }
+}
